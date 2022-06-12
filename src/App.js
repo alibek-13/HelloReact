8594bb81d2707df8ee108483fd07b54dc2e5a709
AlibaIzma <75987408+alibek-13@users.noreply.github.com>
2022-06-12 18:02:00 +0600

    mn be

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">helloreact</div>;
+  const [name, setName] = useState("");
+  const [task, setTask] = useState([]);
+
+  const result = task.map((element, index) => {
+    return <p key={index}>{element}</p>;
+  });
+
+  const remove = (index) => {
+    setTask([...task.filter((obj) => obj.id != index)]);
+  };
+
+  const inputValue = (e) => {
+    setName(e.target.value);
+  };
+
+  const addTask = () => {
+    setTask([...task, name]);
+    setName("");
+  };
+  return (
+    <div>
+      <label>
+        Enter your name:
+        <input type="text" value={name} onChange={inputValue} />
+      </label>
+      <button onClick={addTask}> addTask</button>
+      <div>
+        {result}
+        <button onClick={remove}>Delete</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
